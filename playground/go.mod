6b43dfcff6b86ebea780b652bc32809d4d7e5586
Iván Corrales <ivan.corrales.solera@gmail.com>
2023-08-10 07:27:03 +0200

    Add folder playground to check the usage of the current version of knoa

---
+++ playground/go.mod
@@ -0,0 +1,13 @@
+module github.com/ivancorrales/knoa/playground
+
+go 1.20
+
+require github.com/ivancorrales/knoa v0.0.0
+
+require (
+	github.com/fatih/structs v1.1.0 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
+
+replace github.com/ivancorrales/knoa v0.0.0 => ../
---
+++ playground/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"github.com/ivancorrales/knoa"
+)
+
+type Person struct {
+	Firstname string   `structs:"firstname"`
+	Age       int      `structs:"age"`
+	Siblings  []Person `structs:"siblings,omitempty"`
+}
+
+func main() {
+	k := knoa.Map().Set("firstname", "John", "age", 20)
+	fmt.Println(k.JSON())
+	// {"age":20,"firstname":"John"}
+
+	k.Set("siblings", []Person{
+		{
+			Firstname: "Tim",
+			Age:       29,
+		},
+		{
+			Firstname: "Bob",
+			Age:       40,
+		},
+	})
+	fmt.Println(k.JSON())
+	// {"age":20,"firstname":"John","siblings":[{"age":29,"firstname":"Tim"},{"age":40,"firstname":"Bob"}]}
+
+	k.Set("age", 23, "siblings[1].age", 39)
+	fmt.Println(k.JSON())
+	// {"age":23,"firstname":"John","siblings":[{"age":29,"firstname":"Tim"},{"age":39,"firstname":"Bob"}]}
+
+	k.Set("siblings[*].age", 40)
+	fmt.Println(k.JSON())
+	// {"age":23,"firstname":"John","siblings":[{"age":40,"firstname":"Tim"},{"age":40,"firstname":"Bob"}]}
+
+	var person Person
+	k.To(&person)
+	fmt.Println(person)
+	// {John 23 [{Tim 40 []} {Bob 40 []}]}
+}
